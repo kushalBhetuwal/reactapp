91cbbfce7e74659962f6b4f1cb451becc1fdde4a
kushal <kushalbhetuwal@gmail.com>
2023-07-12 01:16:46 +1000

    Updates

--- src/robot.js
+++
@@ -1,5 +0,0 @@
-export const Robot=[
-    {id:1, name:"kushal", email:"kushalbhetuwal@gmail.com"},
-     {id:2, name:"kate", email:"kate@gmail.com"},
-      {id:3, name:"david", email:"david@gmail.com"}
-]
